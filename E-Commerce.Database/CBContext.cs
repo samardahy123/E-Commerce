5943be1d2f25700fb7025698054f77864e65191b
samardahy <samardahy891@gmaol.com>
2020-11-25 17:05:01 +0200

    create category and product table

---
+++ E-Commerce.Database/CBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using E_Commerce.Entities;
+
+namespace E_Commerce.Database
+{
+   public class CBContext : DbContext
+    {
+        public CBContext() : base("E-CommerceConnection")
+        {
+        }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(50);
+        //}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        //public DbSet<Config> Configurations { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ E-Commerce.Database/Migrations/202011251444280_initialized.cs
@@ -0,0 +1,47 @@
+namespace E_Commerce.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialized : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ImageURL = c.String(),
+                        isFeatured = c.Boolean(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(maxLength: 500),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CategoryID = c.Int(nullable: false),
+                        ImageURL = c.String(),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(maxLength: 500),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Categories", t => t.CategoryID, cascadeDelete: true)
+                .Index(t => t.CategoryID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "CategoryID", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "CategoryID" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
